f89491a53dfaf7983388235176195f94ba65f4b3
Camdon Cady <camdon.cady@gmail.com>
2023-12-27 16:49:35 -0600

    First attempt at scaling

--- 21/solve.py
+++ 21/solve.py
@@ -38,16 +38,51 @@ class Solver():
         return len(reachable_sets[-1])
 
     def solve2(self, steps:int):
-        last_set = set([self.start])
-        for i in range(steps):
-            new_set = set()
-            for (x, y) in last_set:
-                for (i, j) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-                    if (i % self.height, j % self.width) in self.reachable:
-                        new_set.add((i, j))
-            last_set = new_set
+        # Begin with the starting point in the (0, 0) quadrant
+        last_dict: dict[Coord, set[Coord]] = {c: set() for c in self.reachable}
+        last_dict[self.start].add((0, 0))
 
-        return len(last_set)
+        for _ in range(steps):
+            new_dict: dict[Coord, set[Coord]] = {c: set() for c in self.reachable}
+            for (x, y), s in last_dict.items():
+                if len(s) == 0:
+                    continue
+
+                # (x-1, y)
+                if x > 0:
+                    if (x-1, y) in self.reachable:
+                        new_dict[(x-1, y)].update(s)
+                else:
+                    if (self.height-1, y) in self.reachable:
+                        new_dict[(self.height-1, y)].update((i-1, j) for (i, j) in s)
+
+                # (x+1, y)
+                if x+1 < self.height:
+                    if (x+1, y) in self.reachable:
+                        new_dict[(x+1, y)].update(s)
+                else:
+                    if (0, y) in self.reachable:
+                        new_dict[(0, y)].update((i+1, j) for (i, j) in s)
+
+                # (x, y-1)
+                if y > 0:
+                    if (x, y-1) in self.reachable:
+                        new_dict[(x, y-1)].update(s)
+                else:
+                    if (x, self.width-1) in self.reachable:
+                        new_dict[(x, self.width-1)].update((i, j-1) for (i, j) in s)
+
+                # (x, y+1)
+                if y+1 < self.width:
+                    if (x, y+1) in self.reachable:
+                        new_dict[(x, y+1)].update(s)
+                else:
+                    if (x, 0) in self.reachable:
+                        new_dict[(x, 0)].update((i, j+1) for (i, j) in s)
+            
+            last_dict = new_dict
+
+        return sum(len(x) for x in last_dict.values())
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -58,4 +93,4 @@ if __name__ == "__main__":
     solver = Solver(filename)
 
     print(f"First Solution: {solver.solve1(6)}")
-    print(f"Second Solution: {solver.solve2(500)}")
+    print(f"Second Solution: {solver.solve2(1000)}")
